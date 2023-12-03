57f3113795e607e170645b4a8e072f45e7e4baf1
Firuzakerimova <bd7mbuwz1@code.edu.az>
2023-12-03 19:30:57 +0400

    1.JS Intro

---
+++ app.js
@@ -0,0 +1,55 @@
+//Task1
+// let number = prompt("zehmet olmasa UCH reqemli eded daxil edin:");
+// let first = parseInt(number / 100);
+// let second = parseInt((number / 10) % 10);
+// let third = number % 10;
+// console.log(first * 10 + third);
+
+// Task2
+// let mektebli=prompt("mekteblilerin sayini daxil edin");
+// let alma=prompt("almalarin sayini daxil edin");
+// let qalanAlma=alma%mektebli;
+// console.log(qalanAlma);
+
+// Task3
+// let en=parseInt(prompt("en daxil et"));
+// let uzunluq=parseInt(prompt("uzunluq daxil et"));
+// let perimetr=2*(en+uzunluq);
+// console.log(perimetr);
+
+// Task4
+// let a=prompt("a ededini daxil et");
+// let b=prompt("b ededini daxil et");
+// let hasil=a*b;
+// console.log(hasil);
+
+// Task5
+// let a=prompt("a ededini daxil et");
+// let b=prompt("b ededini daxil et");
+// let netice=a**b;
+// console.log(netice);
+
+// Task6
+// let a=prompt("a ededini daxil et");
+// let netice=parseInt (a/10);
+// console.log(netice);
+
+// Task7
+// let a=prompt("a ededini daxil et");
+// let netice=parseInt (a%10);
+// console.log(netice);
+
+// Task8
+// let a=prompt("4 reqemli eded daxil edin:");
+// let first=parseInt(a/1000);
+// let second=(parseInt(a/100))%10;
+// let third=(parseInt(a/10))%10;
+// let fourth=a%10;
+
+// let netice=fourth*1000+third*100+second*10+first;
+// console.log(netice);
+
+
+
+
+
